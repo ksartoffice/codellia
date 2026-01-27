af3704edeca99c408182e24548f55b49465c5b9b
inagaki <inagaki@ksartoffice.com>
2026-01-27 16:51:43 +0900

    feat: Tailwind CSSに関する翻訳を更新し、ツールバーの表示を改善

--- src/admin/toolbar.tsx
+++ src/admin/toolbar.tsx
@@ -107,9 +107,6 @@ const ICONS = {
   }),
 };
 
-const TAILWIND_ICON =
-  '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 54 33"><g clip-path="url(#prefix__clip0)"><path fill="#38bdf8" fill-rule="evenodd" d="M27 0c-7.2 0-11.7 3.6-13.5 10.8 2.7-3.6 5.85-4.95 9.45-4.05 2.054.513 3.522 2.004 5.147 3.653C30.744 13.09 33.808 16.2 40.5 16.2c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C36.756 3.11 33.692 0 27 0zM13.5 16.2C6.3 16.2 1.8 19.8 0 27c2.7-3.6 5.85-4.95 9.45-4.05 2.054.514 3.522 2.004 5.147 3.653C17.244 29.29 20.308 32.4 27 32.4c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C23.256 19.31 20.192 16.2 13.5 16.2z" clip-rule="evenodd"/></g><defs><clipPath id="prefix__clip0"><path fill="#fff" d="M0 0h54v32.4H0z"/></clipPath></defs></svg>';
-
 function IconLabel({ label, svg }: { label: string; svg: string }) {
   return (
     <Fragment>
@@ -171,6 +168,8 @@ function Toolbar({
   const titleText = draftSuffix ? `${resolvedTitle} ${draftSuffix}` : resolvedTitle;
   const titleTooltip = resolvedTitle;
   const normalizedStatus = postStatus === 'auto-draft' ? 'draft' : postStatus;
+  const tailwindBadgeLabel = __( 'Tailwind CSS', 'wp-livecode' );
+  const tailwindTooltip = __( 'Editing in Tailwind CSS mode', 'wp-livecode' );
   const saveLabel =
     normalizedStatus === 'draft'
       ? __( 'Save draft', 'wp-livecode' )
@@ -431,11 +430,12 @@ function Toolbar({
           {tailwindEnabled ? (
             <span
               className="lc-tailwindBadge"
-              title={__( 'TailwindCSS enabled', 'wp-livecode' )}
-              aria-label={__( 'TailwindCSS enabled', 'wp-livecode' )}
-              role="img"
-              dangerouslySetInnerHTML={{ __html: TAILWIND_ICON }}
-            />
+              title={tailwindTooltip}
+              aria-label={tailwindTooltip}
+              data-tooltip={tailwindTooltip}
+            >
+              {tailwindBadgeLabel}
+            </span>
           ) : null}
           {showViewPost ? (
             <a
